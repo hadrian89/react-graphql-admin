3f7004f76eb704f51469a0ebfcc6f7ab1505fe85
Abhinav Srivastav <dev.abhinav89@gmail.com>
2024-12-15 13:51:05 +0000

    gitignore

--- server/graphql/config/database.config.js
+++ server/graphql/config/database.config.js
@@ -2,7 +2,7 @@ module.exports = {
     url: 'mongodb+srv://hadrian:jaisainath123@cluster0.tq7nd.mongodb.net/reading?retryWrites=true&w=majority',
     jwtSecret: "secret",
     email: {
-        user: "vikki.iilm@gmail.com",
-        pass: "test123"
+        user: "reyanshabhinavsri",
+        pass: "arusharvi@123"
     }
 }
